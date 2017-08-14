e20e0ae56c26f24356f3fa16978a490c335bd5ef
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-14 16:35:51 -0300

    Tentar descobrir uma forma de fazer um JavaRDD<Instance> para aplicar as funções de mapeamento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 /**
  * Created by loezerl-fworks on 14/08/17.
  */
+import org.apache.spark.sql.Row;
 import scala.Tuple2;
 
 import org.apache.spark.api.java.JavaPairRDD;
@@ -11,9 +12,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
+//meu
+
+import org.apache.spark.sql.Dataset;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class Main {
-    private static final Pattern SPACE = Pattern.compile(" ");
+    private static final Pattern SPACE = Pattern.compile("\n");
     public static void main(String[] args) throws Exception {
 
 //        if (args.length < 1) {
@@ -27,7 +34,28 @@ public class Main {
                 .config("spark.master", "local")
                 .getOrCreate();
 
-        JavaRDD<String> lines = spark.read().textFile("/home/loezerl-fworks/IdeaProjects/spark-knn/aux/lucas.txt").javaRDD();
+
+        DataSource source = new DataSource("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff");
+
+        Instances data = source.getDataSet();
+
+        if(data.classIndex() == -1){
+            data.setClassIndex(data.numAttributes() - 1);
+        }
+
+//        JavaRDD<Row> dataset_ = spark.read().csv("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff").javaRDD();
+//
+//        List<Row> output__ = dataset_.collect();
+//
+//        for (Row linha : output__){
+//            System.out.println(linha.toString());
+//        }
+
+        JavaRDD<Instance> data__;
+
+        
+
+        JavaRDD<String> lines = spark.read().textFile("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff").javaRDD();
 
         JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
 
