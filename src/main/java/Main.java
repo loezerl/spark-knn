d7546f0d1f0865257e830bed357fb06188f97fa1
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-17 12:27:46 -0300

    Consegui criar um javaRDD com a classe Instance. O unico problema é criar uma lista do tipo Instance e ai converter para javaRDD, talvez seja uma opcao boa fazer um InstanceGenerator.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
 /**
  * Created by loezerl-fworks on 14/08/17.
  */
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.codehaus.janino.Java;
 import scala.Tuple2;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 import java.util.regex.Pattern;
 
 //meu
 
 import org.apache.spark.sql.Dataset;
+import util.Similarity;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -28,44 +34,54 @@ public class Main {
 //            //System.exit(1);
 //        }
 
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("JavaWordCount")
-                .config("spark.master", "local")
-                .getOrCreate();
+//        SparkSession spark = SparkSession
+//                .builder()
+//                .appName("JavaWordCount")
+//                .config("spark.master", "local")
+//                .getOrCreate();
+
+        SparkConf conf = new SparkConf().setAppName("org.sparkexample.WordCount").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
 
+        ///////////////////////////////////////// WEKA ARFF LOADER
         DataSource source = new DataSource("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff");
-
         Instances data = source.getDataSet();
-
         if(data.classIndex() == -1){
             data.setClassIndex(data.numAttributes() - 1);
         }
 
-//        JavaRDD<Row> dataset_ = spark.read().csv("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff").javaRDD();
-//
-//        List<Row> output__ = dataset_.collect();
-//
-//        for (Row linha : output__){
-//            System.out.println(linha.toString());
-//        }
+        Instance auxI = data.get(10);
+        ////////////////////////
+        List<Instance> ListInst = new Vector<>();
 
-        JavaRDD<Instance> data__;
-
-        JavaRDD<String> lines = spark.read().textFile("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff").javaRDD();
-
-        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-
-        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?,?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
+        for (int i =0; i< data.numInstances(); i++){
+            ListInst.add(data.get(i));
         }
-        spark.stop();
+
+        JavaRDD<Instance> dataset = sc.parallelize(ListInst);
+
+        JavaPairRDD<Instance, Double> distances = dataset.mapToPair(s -> new Tuple2<>(s, Similarity.EuclideanDistance(auxI, s)));
+
+        List<Tuple2<Instance, Double>> output = distances.collect();
+
+        for (Tuple2<?,?> tuple : output){
+            System.out.println(tuple._1() + " : " + tuple._2());
+        }
+
+//        JavaRDD<String> lines = spark.read().textFile("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff").javaRDD();
+//
+//        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+//
+//        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+//
+//        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+//
+//        List<Tuple2<String, Integer>> output = counts.collect();
+////        for (Tuple2<?,?> tuple : output) {
+////            System.out.println(tuple._1() + ": " + tuple._2());
+////        }
+//        spark.stop();
     }
 
 }
--- src/main/java/util/InstanceGenerator.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-/**
- * Created by loezerl-fworks on 14/08/17.
- */
-public class InstanceGenerator {
-}
-
-// https://stackoverflow.com/questions/41668340/convert-class-object-to-java-rdd
