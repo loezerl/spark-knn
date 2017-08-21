a40814c15200e768c9c5eb1a30f94d76c4961abc
Loezer-LAJES <loezer.lucas@gmail.com>
2017-08-21 16:19:10 -0300

    Alguns testes para tentar carregar um arquivo arff grande, todos insucessos. Deixarei isso para a segunda iteração.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Repositório responsável pela criação do KNN dentro do framework Spark.
 - Experimenter: Parser dos parametros.
 - Descrever as funcionalidades de cada função.
 - Ver como carrega um arff gigante com as bibliotecas do MOA.
+
+Então, vou deixar a parte de implementar e modularizar cada experimenter(em cada framework) para a segunda iteração, principalmente tratar os arquivos grandes.
--- src/main/java/Experimenter.java
+++ src/main/java/Experimenter.java
@@ -11,16 +11,24 @@ import evaluators.Prequential;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import weka.core.Instances;
+import weka.core.converters.ArffLoader;
 import weka.core.converters.ConverterUtils.DataSource;
 
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.regex.Pattern;
+
+import org.apache.spark.streaming.*;
+import org.apache.spark.streaming.api.java.*;
 
 public class Experimenter {
     private static final Pattern SPACE = Pattern.compile("\n");
     public static SparkConf conf;
     public static JavaSparkContext sc;
-    
+
     public static void main(String[] args) throws Exception {
 
         //        if (args.length < 1) {
@@ -33,19 +41,56 @@ public class Experimenter {
 //                .appName("JavaWordCount")
 //                .config("spark.master", "local")
 //                .getOrCreate();
-
-        //local[2] - seta o numero de threads
+//
+//        local[2] - seta o numero de threads
         conf = new SparkConf().setAppName("spark-knn").setMaster("local[4]").set("spark.cores.max", "4");
         sc = new JavaSparkContext(conf);
         sc.setLogLevel("ERROR");
 
+        //JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+
+//        FileInputStream inputStream = null;
+//        Scanner sc = null;
+//        try {
+//            inputStream = new FileInputStream("/home/loezerl-fworks/Downloads/kyoto.arff");
+//            sc = new Scanner(inputStream, "UTF-8");
+//            while (sc.hasNextLine()) {
+//                String line = sc.nextLine();
+//                System.out.println(line);
+//            }
+//            // note that Scanner suppresses exceptions
+//            if (sc.ioException() != null) {
+//                throw sc.ioException();
+//            }
+//        } finally {
+//            if (inputStream != null) {
+//                inputStream.close();
+//            }
+//            if (sc != null) {
+//                sc.close();
+//            }
+//        }
+
         ///////////////////////////////////////// WEKA ARFF LOADER
-        DataSource source = new DataSource("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff");
-        Instances data = source.getDataSet();
+//        DataSource source = new DataSource("/home/loezerl-fworks/IdeaProjects/Experimenter/diabetes.arff");
+//        Instances data = source.getDataSet();
+//        if (data.classIndex() == -1) {
+//            data.setClassIndex(data.numAttributes() - 1);
+//        }
+        ////////////////////////
+
+        ////////////////// WEKA ARFF LOAD 2
+        BufferedReader reader =
+                new BufferedReader(new FileReader("/home/loezerl-fworks/Downloads/kyoto.arff"));
+        System.out.println("Reader criado");
+        ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
+        System.out.println("Arff Reader Criado");
+        Instances data = arff.getData();
         if (data.classIndex() == -1) {
             data.setClassIndex(data.numAttributes() - 1);
         }
-        ////////////////////////
+        ///////////////////////////////////////////////
 
         Classifier myClassifier = new KNN(7, 30, "euclidean", sc);
         Evaluator myEvaluator = new Prequential(myClassifier, data);
